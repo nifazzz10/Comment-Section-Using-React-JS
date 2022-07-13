d1f136e22b9ddbc1a6efde28cb56d2b61335cd98
nifazzz10 <nifazzz10@gmail.com>
2022-07-13 18:16:01 +0530

    New Comment section

--- src/Mtt.js
+++ src/Mtt.js
@@ -1,45 +1,87 @@
-import ReactDOM from "react-dom";
-import React from "react";
-import TreeView from "@mui/lab/TreeView";
-import TreeItem from "@mui/lab/TreeItem";
-import { makeStyles, withStyles } from "@material-ui/core/styles";
-
-const { useState, useCallback } = React;
-
-const useStyles = makeStyles({
-  "@global": {
-    ".MuiTreeItem-root.Mui-selected > .MuiTreeItem-content .MuiTreeItem-label": {
-      backgroundColor: "white"
-    },
-    ".MuiTreeItem-root.Mui-selected > .MuiTreeItem-content .MuiTreeItem-label:hover, .MuiTreeItem-root.Mui-selected:focus > .MuiTreeItem-content .MuiTreeItem-label": {
-      backgroundColor: "gray"
-    }
-  }
-});
-
-export default function MyTreeItem(props) {
+// /* 
+//  * Simple editor component that takes placeholder text as a prop 
+//  */
+// import React from 'react';
+// import PropTypes from 'prop-types'
+// import ReactQuill from "react-quill";
+// import "react-quill/dist/quill.snow.css";
+// import "react-quill/dist/quill.bubble.css";
+// import { ReactDOM } from 'react';
+//  export default class Editor extends React.Component {
+// 	constructor (props) {
+// 	  super(props)
+// 	  this.state = { editorHtml: '', theme: 'snow' }
+// 	  this.handleChange = this.handleChange.bind(this)
+// 	}
+	
+// 	handleChange (html) {
+// 		this.setState({ editorHtml: html });
+// 	}
+	
+// 	handleThemeChange (newTheme) {
+// 	  if (newTheme === "core") newTheme = null;
+// 	  this.setState({ theme: newTheme })
+// 	}
+	
+// 	render () {
+// 	  return (
+// 		<div>
+// 		  <ReactQuill 
+// 			theme="bubble"
+// 			onChange={this.handleChange}
+// 			value={this.state.editorHtml}
+// 			modules={Editor.modules}
+// 			formats={Editor.formats}
+// 			bounds={'.app'}
+// 			placeholder={this.props.placeholder}
+// 		   />
   
-  const classes = useStyles();
-
-  return (
-    <div>
-      <TreeView
-    
-    >
-      <TreeItem nodeId="1" label="Applications">
-        <TreeItem nodeId="2" label="Calendar" />
-        <TreeItem nodeId="3" label="Chrome" />
-        <TreeItem nodeId="4" label="Webstorm" />
-      </TreeItem>
-      <TreeItem nodeId="5" label="Documents">
-        <TreeItem nodeId="6" label="MUI">
-          <TreeItem nodeId="7" label="src">
-            <TreeItem nodeId="8" label="index.js" />
-            <TreeItem nodeId="9" label="tree-view.js" />
-          </TreeItem>
-        </TreeItem>
-      </TreeItem>
-    </TreeView>
-    </div>
-  );
-}
+// 		 </div>
+// 	   )
+// 	}
+//   }
+  
+//   /* 
+//    * Quill modules to attach to editor
+//    * See https://quilljs.com/docs/modules/ for complete options
+//    */
+//   Editor.modules = {
+// 	toolbar: [
+// 	  [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
+// 	  [{size: []}],
+// 	  ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+// 	  [{'list': 'ordered'}, {'list': 'bullet'}, 
+// 	   {'indent': '-1'}, {'indent': '+1'}],
+// 	  ['link', 'image', 'video'],
+// 	  ['clean']
+// 	],
+// 	clipboard: {
+// 	  // toggle to add extra line breaks when pasting HTML:
+// 	  matchVisual: false,
+// 	}
+//   }
+//   /* 
+//    * Quill editor formats
+//    * See https://quilljs.com/docs/formats/
+//    */
+//   Editor.formats = [
+// 	'header', 'font', 'size',
+// 	'bold', 'italic', 'underline', 'strike', 'blockquote',
+// 	'list', 'bullet', 'indent',
+// 	'link', 'image', 'video'
+//   ]
+  
+//   /* 
+//    * PropType validation
+//    */
+//   Editor.propTypes = {
+// 	placeholder: PropTypes.string,
+//   }
+  
+//   /* 
+//    * Render component on page
+//    */
+//   ReactDOM.render(
+// 	<Editor placeholder={'Write something...'}/>, 
+// 	document.querySelector('.app')
+//   )
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import editor from "./Mtt"
 ReactDOM.render(
   <React.StrictMode>
     <App />
     {/* <MyTreeItem /> */}
-    {/* <GetJson /> */}
+{/* <Editor /> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
